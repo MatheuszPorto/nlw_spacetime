6a640192aa0d874968ff810905bfc28d66850566
Matheus Mendonça <matthmen16@gmail.com>
2023-05-16 22:17:21 -0300

    initial commit

---
+++ web/src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/Button";
+
+export default function Home() {
+  return (
+    <h1>
+      Hello World
+      <Button title= "Matheus"/>
+      <Button title= "Teff"/>
+      <Button title= "Manel"/>
+    </h1>
+  )
+};
